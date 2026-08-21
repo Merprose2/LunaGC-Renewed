2e82f31be7f85fd21afde8df408c38cc536c2410
Merprose <95530702+Merprose2@users.noreply.github.com>
2026-08-21 21:12:35 +0200

    Update comments in SetUpLunchBoxWidgetReqOuterClass

--- src/generated/main/java/emu/grasscutter/net/proto/SetUpLunchBoxWidgetReqOuterClass.java
+++ src/generated/main/java/emu/grasscutter/net/proto/SetUpLunchBoxWidgetReqOuterClass.java
@@ -34,6 +34,10 @@ public final class SetUpLunchBoxWidgetReqOuterClass {
     emu.grasscutter.net.proto.LunchBoxDataOuterClass.LunchBoxDataOrBuilder getLunchBoxDataOrBuilder();
   }
   /**
+   * <pre>
+   * CmdID: 5446
+   * </pre>
+   *
    * Protobuf type {@code SetUpLunchBoxWidgetReq}
    */
   public static final class SetUpLunchBoxWidgetReq extends
@@ -310,6 +314,10 @@ public final class SetUpLunchBoxWidgetReqOuterClass {
       return builder;
     }
     /**
+     * <pre>
+     * CmdID: 5446
+     * </pre>
+     *
      * Protobuf type {@code SetUpLunchBoxWidgetReq}
      */
     public static final class Builder extends
