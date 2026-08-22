74a763c796a5cad9585ddbc59ce57ec49461469e
Merprose <95530702+Merprose2@users.noreply.github.com>
2026-08-22 19:10:37 +0200

    Fix cache modification time check in Language.java

--- src/main/java/emu/grasscutter/utils/lang/Language.java
+++ src/main/java/emu/grasscutter/utils/lang/Language.java
@@ -332,8 +332,10 @@ public final class Language {
         if (!bypassCache)
             try {
                 long cacheModified = Files.getLastModifiedTime(TEXTMAP_CACHE_PATH).toMillis();
-                long textmapsModified =
-                        Files.list(getResourcePath("TextMap"))
+                long textmapsModified;
+                try (var textmaps = Files.list(getResourcePath("TextMap"))) {
+                    textmapsModified =
+                        textmaps
                                 .filter(path -> path.toString().endsWith(".json"))
                                 .map(
                                         path -> {
@@ -347,6 +349,7 @@ public final class Language {
                                         })
                                 .max(Long::compare)
                                 .get();
+                }
 
                 Grasscutter.getLogger()
                         .debug(
